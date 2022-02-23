27b39e22ced1d690d6fdd41dcd57844d4d22f92c
prmacedo <p.ricardomacedo@hotmail.com>
2022-02-22 23:11:20 -0300

    return to previous configuration

--- src/controllers/documentController.js
+++ src/controllers/documentController.js
@@ -243,90 +243,87 @@ router.delete('/document/:id', async (req, res) => {
   }
 });
 
-// router.get('/download/proposal/pdf/:id', async (request, response) => {
-//   const { id } = request.params;
+router.get('/download/proposal/pdf/:id', async (request, response) => {
+  const { id } = request.params;
 
-//   const browser = await puppeteer.launch()
-//   const page = await browser.newPage()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-//   // page.setExtraHTTPHeaders({
-//   //   'authorization': request.headers.authorization
-//   // })
+  page.setExtraHTTPHeaders({
+    'authorization': request.headers.authorization
+  })
 
-//   const urlToDownload = `${process.env.SERVER_PDF_URL}/generate/proposal/pdf/${id}`;
+  const urlToDownload = `${process.env.SERVER_PDF_URL}/generate/proposal/pdf/${id}`;
 
-//   await page.goto(urlToDownload, {
-//     waitUntil: 'networkidle0'
-//   })
+  await page.goto(urlToDownload, {
+    waitUntil: 'networkidle0'
+  })
 
-//   const name = 'Proposta.pdf'
+  const name = 'Proposta.pdf'
 
-//   const pdf = await page.pdf({
-//     printBackground: true,
-//     format: 'Letter',
-//     path: "./src/proposta.pdf"
-//   })
+  const pdf = await page.pdf({
+    printBackground: true,
+    format: 'Letter',
+    path: "./src/proposta.pdf"
+  })
 
-//   await browser.close()
+  await browser.close()
 
-//   response.contentType("application/pdf")
+  response.contentType("application/pdf")
   
-//   const filePath = path.join(__dirname, "../", "proposta.pdf");
+  const filePath = path.join(__dirname, "../", "proposta.pdf");
 
-//   // return response.send(pdf)
+  // return response.send(pdf)
 
-//   return response.download(filePath, name, (err) => {
-//     if (err)
-//       console.log(err)
+  return response.download(filePath, name, (err) => {
+    if (err)
+      console.log(err)
     
-//     console.log("ok");
-//   });
-// })
+    console.log("ok");
+  });
+})
 
-// router.get('/generate/proposal/pdf/:id', async (request, response) => {
-//   const { id } = request.params;
+router.get('/generate/proposal/pdf/:id', async (request, response) => {
+  const { id } = request.params;
 
-//   try {
-//     const document = await prisma.document.findUnique({
-//       include: {
-//         client: true,
-//         assignee: true
-//       },
-//       where: {
-//         id: Number(id)
-//       }
-//     });
+  try {
+    const document = await prisma.document.findUnique({
+      include: {
+        client: true,
+        assignee: true
+      },
+      where: {
+        id: Number(id)
+      }
+    });
   
-//     const formatNumber = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
+    const formatNumber = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
 
-//     document.proposalDate = (new Date(document.proposalDate)).toLocaleDateString('pt-BR')
+    document.proposalDate = (new Date(document.proposalDate)).toLocaleDateString('pt-BR')
     
-//     document.value = formatNumber.format(document.value).split(" ")[1]
-//     document.correction = formatNumber.format(document.correction).split(" ")[1]
-//     document.fee = formatNumber.format(document.fee).split(" ")[1]
-//     document.preference = formatNumber.format(document.preference).split(" ")[1]
-//     document.taxes = formatNumber.format(document.taxes).split(" ")[1]
-//     document.percentage = formatNumber.format(document.percentage).split(" ")[1]
-//     document.updatedValue = formatNumber.format(document.updatedValue).split(" ")[1]
-//     document.liquidValue = formatNumber.format(document.liquidValue).split(" ")[1]
-//     document.proposalValue = formatNumber.format(document.proposalValue).split(" ")[1]
+    document.value = formatNumber.format(document.value).split(" ")[1]
+    document.correction = formatNumber.format(document.correction).split(" ")[1]
+    document.fee = formatNumber.format(document.fee).split(" ")[1]
+    document.preference = formatNumber.format(document.preference).split(" ")[1]
+    document.taxes = formatNumber.format(document.taxes).split(" ")[1]
+    document.percentage = formatNumber.format(document.percentage).split(" ")[1]
+    document.updatedValue = formatNumber.format(document.updatedValue).split(" ")[1]
+    document.liquidValue = formatNumber.format(document.liquidValue).split(" ")[1]
+    document.proposalValue = formatNumber.format(document.proposalValue).split(" ")[1]
 
-//     const filePath = path.join(__dirname, "../", "reports", "proposal.ejs")
-//     ejs.renderFile(filePath, { document }, (err, html) => {
-//       if (err) {
-//         return response.send('Erro na leitura do arquivo')
-//       }
+    const filePath = path.join(__dirname, "../", "reports", "proposal.ejs")
+    ejs.renderFile(filePath, { document }, (err, html) => {
+      if (err) {
+        return response.send('Erro na leitura do arquivo')
+      }
 
-//       // enviar para o navegador
-//       return response.send(html)
-//     })
-//   } catch (error) {
-//     return response.send({ error: error.message });
-//   }
-  
-  
-
-// })
+      // enviar para o navegador
+      return response.send(html)
+    })
+  } catch (error) {
+    return response.send({ error: error.message });
+  }  
+})
 
 router.get('/download/contract/pdf/:id', async (request, response) => {
   const { id } = request.params;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
-const ejs = require('ejs')
-const path = require('path')
-const puppeteer = require('puppeteer')
 
 const app = express();
-const prisma = new PrismaClient();
 
 const corsOptions = {
   allowedHeaders: ['Authorization', 'Content-Type', 'Content-Length'],
-  origin: true,
+  origin: process.env.CORS_ORIGIN_URL,
   methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true
 }
@@ -26,88 +22,6 @@ app.options('*', cors());
 app.get('/', (req, res) => {
   res.send({ ok: true });
 });
-
-app.get('/generate/proposal/pdf/:id', async (request, response) => {
-  const { id } = request.params;
-
-  try {
-    const document = await prisma.document.findUnique({
-      include: {
-        client: true,
-        assignee: true
-      },
-      where: {
-        id: Number(id)
-      }
-    });
-
-    const formatNumber = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
-
-    document.proposalDate = (new Date(document.proposalDate)).toLocaleDateString('pt-BR')
-
-    document.value = formatNumber.format(document.value).split(" ")[1]
-    document.correction = formatNumber.format(document.correction).split(" ")[1]
-    document.fee = formatNumber.format(document.fee).split(" ")[1]
-    document.preference = formatNumber.format(document.preference).split(" ")[1]
-    document.taxes = formatNumber.format(document.taxes).split(" ")[1]
-    document.percentage = formatNumber.format(document.percentage).split(" ")[1]
-    document.updatedValue = formatNumber.format(document.updatedValue).split(" ")[1]
-    document.liquidValue = formatNumber.format(document.liquidValue).split(" ")[1]
-    document.proposalValue = formatNumber.format(document.proposalValue).split(" ")[1]
-
-    const filePath = path.join(__dirname, "reports", "proposal.ejs")
-    ejs.renderFile(filePath, { document }, (err, html) => {
-      if (err) {
-        return response.send('Erro na leitura do arquivo')
-      }
-
-      // enviar para o navegador
-      return response.send(html)
-    })
-  } catch (error) {
-    return response.send({ error: error.message });
-  }
-})
-
-app.get('/download/proposal/pdf/:id', async (request, response) => {
-  const { id } = request.params;
-
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-
-  // page.setExtraHTTPHeaders({
-  //   'authorization': request.headers.authorization
-  // })
-
-  const urlToDownload = `${process.env.SERVER_PDF_URL}/generate/proposal/pdf/${id}`;
-
-  await page.goto(urlToDownload, {
-    waitUntil: 'networkidle0'
-  })
-
-  const name = 'Proposta.pdf'
-
-  const pdf = await page.pdf({
-    printBackground: true,
-    format: 'Letter',
-    path: "./src/proposta.pdf"
-  })
-
-  await browser.close()
-
-  response.contentType("application/pdf")
-
-  const filePath = path.join(__dirname, "proposta.pdf");
-
-  // return response.send(pdf)
-
-  return response.download(filePath, name, (err) => {
-    if (err)
-      console.log(err)
-
-    console.log("ok");
-  });
-})
 
 require('./controllers/authController')(app);
 
